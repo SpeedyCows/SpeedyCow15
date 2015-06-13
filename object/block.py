1ca881ff41995061c177e91dff516ebd680c7964
Ralphthulhu <ralphthulhu@user.noreply.github.com>
2015-06-13 18:09:57 -0400

    Fixing block movement (somewhat)

--- object/block.py
+++ object/block.py
@@ -15,33 +15,40 @@ class Block(Object):  # represents the water, not the game
         self.width = 1
         
         self.direction = 0
-        self.xSpeed = 0
-        self.ySpeed = 0
+        self.xSpeed = Block.MAX_SPEED
+        self.ySpeed = Block.MAX_SPEED
         
     def collide(self, object):
-        distance = 2
+
         self.old_x = self.x
         self.old_y = self.y
         if type(object).__name__ == 'Player_Ant':
             self.direction = object._direction
             if object._direction == 0:
-                self.x += distance               
+                self.x += self.xSpeed         
             elif object._direction == 90:
-                self.y -= distance
+                self.y -= self.ySpeed
+                self.ySpeed = Block.MAX_SPEED
             elif object._direction == 180:
-                self.x -= distance
+                self.x -= self.xSpeed
             elif object._direction == 270:
-                self.y += distance
-            object.collide(self)
+                self.y += self.ySpeed
             
         elif type(object) is Dirt:
             if self.direction == 0 and object.x < self.x + self.dimension:
                 self.x = object.x - self.dimension
+                self.xSpeed = 0
             elif self.direction == 90 and object.y + object.dimension > self.y:
                 self.y = object.y + object.dimension
+                self.ySpeed = 0
             elif self.direction == 180 and object.x + object.dimension > self.x:
                 self.x = object.x + object.dimension
+                self.xSpeed = 0
             elif self.direction == 270 and object.y < self.y + self.dimension:
                 self.y = object.y - self.dimension
-                
+                self.ySpeed
         return      
+        
+    def inBetweenLoops(self):
+        self.xSpeed = Block.MAX_SPEED
+        self.ySpeed = Block.MAX_SPEED
