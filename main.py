49dfcc817b0ecb7041e1335f1d2bfb2907f81960
josh <joshualilly@me.com>
2015-06-13 14:50:59 -0400

    Added crazy ant

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from object.object import *
 from object.player_ant import *
 from object.water import Water
 from object.dirt import Dirt
+from object.crazyant import CrazyAnt
 
 SQUARE_SIZE = 40
 
@@ -22,6 +23,11 @@ def main():
     object2 = Object(SQUARE_SIZE)
     object2.setPos(280, 280)
     objects.append(object2)
+    crazyAnt = CrazyAnt(SQUARE_SIZE, object1)
+    crazyAnt.setPos(500, 500)
+    objects.append(crazyAnt)
+    
+    
     #flag = 0
     #for x in xrange(800):
 	#for y in xrange(600):
@@ -41,7 +47,7 @@ def main():
             if not (y == 0 and x == 0) and not (x == 7 and y == 7):
                 dirt = Dirt(SQUARE_SIZE)
                 dirt.setPos(x * SQUARE_SIZE, y * SQUARE_SIZE)
-                objects.append(dirt)
+              #  objects.append(dirt)
     print "[DEBUG] Done Setting up world"
 
     running = True
@@ -64,6 +70,8 @@ def main():
                 if (object3 != object4):
                     if (object3.check_collision(object4)):
                         object3.collide(object4)
+                crazyAnt.searchForPlayer()
+                crazyAnt.draw(screen)
             object3.draw(screen)
 
         #ant.draw(screen) # draw the bird to the screen
