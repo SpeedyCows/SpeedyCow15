baeab2b77b17913c55e70628e2378ff3c871f335
Ralphthulhu <ralphthulhu@user.noreply.github.com>
2015-06-13 14:28:34 -0400

    Added tunneling

--- object/dirt.py
+++ object/dirt.py
@@ -7,10 +7,12 @@ class Dirt(Object):  # represents the water, not the game
         super(Dirt, self).__init__(dimension)
 
 	# Option 1 - Loads fast
-	self.red = 68
-	self.green = 26
-	self.blue = 26
-	self.width = 0
+        self.red = 68
+        self.green = 26
+        self.blue = 26
+        self.width = 0
+    
+        self.empty = False
 
 	# Option 2 - Loads slow
 	#self.image = pygame.image.load('images/dirt.jpg')
@@ -18,3 +20,11 @@ class Dirt(Object):  # represents the water, not the game
 	# Option 3 - Loads ok, but images quality reduced
         #self.image = pygame.image.load('images/dirt_resized.jpg')
         #self.image = pygame.transform.scale(self.image, (dimension, dimension))
+
+    def collide(self, object):
+        if type(object).__name__ == 'Player_Ant':
+            self.red = 68
+            self.green = 26
+            self.blue = 26
+            self.width = 0
+            self.empty = True
