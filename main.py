6f1cf93d7a2a2bfbeab622dfe1418d6ac8c93b3c
david.thacher <davidethacher@gmail.com>
2015-06-14 17:02:25 -0400

    Updated sugar generation

--- main.py
+++ main.py
@@ -69,7 +69,6 @@ def main():
 
     # Set Difficulty
     dif = 'e'
-    genSugar = 0
     #make background dirt
     topdirt = pygame.image.load("images/dirt1.png")
     topdirt = pygame.transform.scale(topdirt, (BLOCK_SIZE, BLOCK_SIZE))
@@ -129,16 +128,13 @@ def main():
             staticObjects.append(egg)
             eggTimer = t
             randomEggSpawnTime = queen.getRandomEggTime()
-        if (t - sugarTimer) > 30 and genSugar >= 100:
+        if (t - sugarTimer) > 30:
             sugar = Sugar(rand.choice((SQUARE_SIZE, SQUARE_SIZE / 2)))
             randomX = rand.randint(100, 800 - SQUARE_SIZE)
             randomY = rand.randint(100, 600 - SQUARE_SIZE)
             sugar.setPos(randomX, randomY)
             staticObjects.append(sugar)
-            genSugar = 0
-        else:
-            genSugar += 1
-            sugar = t
+            sugarTimer = t
         if (t - leafTimer) > 15:
             leaf = Leaf(rand.choice((SQUARE_SIZE, SQUARE_SIZE / 2)))
             randomX = rand.randint(100, 800 - SQUARE_SIZE)
