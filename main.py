dd13b64e361e1d5814b22106f868d79f528d23e9
DepletionSlayer <DS@bgj.noreply.com>
2015-06-14 00:22:16 -0400

    Fixed movement, added up

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ def processPYGame(ant, keycount):
             #only pause if num of key down is now 0
             keycount -= 1
             if keycount <= 0:
+                keycount = 0
                 ant.pause_ani()
         elif event.type == pygame.MOUSEMOTION:
             ant.handle_mouse(event)
